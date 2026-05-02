423fb931b6dd50279b6d7d51d09431aa45f5e635
nayminthumbg-ops <nayminthu.mbg@gmail.com>
2026-05-02 14:38:14 +0630

    Clean up commented import statements
    Removed commented lines for local usage in VS Code.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// Local (VS Code) တွင် အသုံးပြုရန် အောက်ပါ ၂ ကြောင်း၏ ရှေ့မှ // များကို ဖျက်၍ အသုံးပြုပါ။
-import App from './App.jsx'
-import './index.css'
 
 // ဤသည်မှာ Canvas တွင် Error မပြစေရန် ရေးသားထားသော ယာယီကုဒ်ဖြစ်ပါသည်။
 const App = () => (
